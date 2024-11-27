b873d04efd2df710156fa8253f3a1bd62eb3478a
sangred <vudinhsang184@gmail.com>
2024-11-27 17:46:27 +0700

    sang

--- admin_edit.php
+++ admin_edit.php
@@ -11,58 +11,57 @@
 
 <body>
     <?php include 'admin_navbar.php'; ?>
-
+        
     <?php
-    $account = [
-        'username' => 'chihihi',
-        'first_name' => 'Tran',
-        'last_name' => 'Chi',
-        'email' => 'chihihi@gmail.com',
-        'status' => 'Active'
-    ];
+    require 'connect.php';
+    $thucuong = $_GET['mathucuong'];
+    $sql = "SELECT * FROM dsthucuong where mathucuong='$thucuong' ";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) :
+        $dsthucuong = $result->fetch_all(MYSQLI_ASSOC);
+        
+        $thucuong  = $dsthucuong[0];
     ?>
-
     
 
+
+
+    
     <div class="container">
 
         <div class="card">
-            <H1>CẬP NHẬT TÀI KHOẢN</H1>
+            <H1>form edit thuc uong</H1>
 
-            <form>
+            <form action ="admin_edit_action.php" method= "post">
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Tên đăng nhập</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" value="<?= $account['username'] ?>">
+                    <label for="exampleInputEmail1" class="form-label">Mã Thức Uống: </label>
+                    <input type="text" class="form-control" id="mathucuong" name="mathucuong"value="<?= $thucuong['mathucuong'] ?>">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Họ</label>
-                    <input type="text" class="form-control" id="exampleInputPassword1" value="<?= $account['first_name'] ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Tên:</label>
-                    <input type="text" class="form-control" id="exampleInputPassword1" value="<?= $account['last_name'] ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="exampleInputPassword1" value="<?= $account['email'] ?>">
-                </div>
+                    <label for="exampleInputPassword1" class="form-label">Hình Ảnh: </label>
+                    <input type="text" class="form-control" id="" value="<?= $thucuong['hinhanh'] ?>">
+                    <img src="<?= $thucuong['hinhanh'] ?>" alt=""hinhanh>
 
+                </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Trạng thái:</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Vui lòng chọn...</option>
-                        <option value="1"
-                            <?= ($account['status'] == 'Active') ? 'selected' : '' ?>>
-                            Kích hoạt</option>
-                        <option value="2"
-                            <?= ($account['status'] == 'Disable') ? 'selected' : '' ?>>
-                            Vô hiệu hóa</option>
-                    </select>
+                    <label for="exampleInputPassword1" class="form-label">Tên: </label>
+                    <input type="text" class="form-control" id="ten" name="ten" value="<?= $thucuong['ten'] ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Mô Tả: </label>
+                    <input type="text" class="form-control" id="mota" name="mota" value="<?= $thucuong['mota'] ?>">
                 </div>
 
-                <button type="submit" class="btn btn-primary">Gửi</button>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Giá:</label>
+                    <input type="number" class="form-control" id="gia" name="gia" value="<?= $thucuong['gia'] ?>">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Sửa</button>
             </form>
         </div>
     </div>
-
+    <?php
+    endif;
+    ?>
 </body>
